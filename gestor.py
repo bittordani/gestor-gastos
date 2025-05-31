458278373ebde3e5d197f2290328f017253453b2
bittordani <victordani.martinez@gmail.com>
2025-05-31 20:14:19 +0200

    Añadir main con FastAPI y script de prueba. Revisión de comentarios y eliminar script de test

--- gestor.py
+++ gestor.py
@@ -1,14 +1,25 @@
 from gasto import Gasto
 from ingreso import Ingreso
 
-#
+# Clase GestorGastos donde gestionamos los métodos de agregar y eliminar movimientos
+# y de listar el saldo total
 class GestorGastos:
     def __init__(self):
         self.movimientos = []
+        self._id_movimiento = 1
 
     def agregar(self, movimiento):
+        movimiento.id = self._id_movimiento
+        self._id_movimiento += 1
         self.movimientos.append(movimiento)
 
+    def eliminar(self, id):
+        for i in self.movimientos:
+            if i.id == id:
+                self.movimientos.remove(i)
+                return True
+        return False    
+
     def saldo_total(self):
-        return sum(m.importe() for m in self.movimientos)
+        return sum(i.importe() for i in self.movimientos)
     
